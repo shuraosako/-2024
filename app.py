3fd3d670cb47fa85243d52ddd269b33b00d5b14f
Shura <s21m1012@bene.fit.ac.jp>
2024-12-06 02:01:05 +0900

    update

--- app.py
+++ app.py
@@ -2,34 +2,61 @@ from flask import Flask, request, send_file
 from PIL import Image
 import io
 import numpy as np
-# CycleGANモデルをインポートする
-# from cyclegan_model import CycleGANModel
+import torch
+from models.cyclegan import Generator
 
 app = Flask(__name__)
 
-# CycleGANモデルのインスタンスを作成
-# model = CycleGANModel()
+MODEL_PATH = {
+    'photo_to_ukiyoe': 'models/photo_to_ukiyoe.pth',
+    'ukiyoe_to_photo': 'models/ukiyoe_to_photo.pth'
+}
+
+photo_to_ukiyoe = Generator()
+ukiyoe_to_photo = Generator()
+
+photo_to_ukiyoe.load_state_dict(torch.load(MODEL_PATH['photo_to_ukiyoe'], map_location=torch.device('cpu')))
+ukiyoe_to_photo.load_state_dict(torch.load(MODEL_PATH['ukiyoe_to_photo'], map_location=torch.device('cpu')))
+
+photo_to_ukiyoe.eval()
+ukiyoe_to_photo.eval()
+
+def preprocess_image(img):
+    img = torch.from_numpy(img.transpose((2, 0, 1))).float()
+    img = img.unsqueeze(0) / 255.0
+    img = (img - 0.5) / 0.5
+    return img
+
+def postprocess_image(tensor):
+    img = (tensor + 1) / 2
+    img = img.squeeze().detach().numpy()
+    img = img.transpose(1, 2, 0) * 255.0
+    return img
 
 @app.route('/transform', methods=['POST'])
 def transform_image():
     if 'image' not in request.files:
         return 'No image file provided', 400
     
+    direction = request.form.get('direction', 'photo_to_ukiyoe')
     file = request.files['image']
     img = Image.open(file.stream)
     
-    # 画像をNumPy配列に変換
+    if img.mode != 'RGB':
+        img = img.convert('RGB')
+    
     img_array = np.array(img)
+    img_tensor = preprocess_image(img_array)
     
-    # CycleGANモデルを使用して画像を変換
-    # transformed_img_array = model.transform(img_array)
-    # 仮の処理：元の画像をそのまま返す
-    transformed_img_array = img_array
+    with torch.no_grad():
+        if direction == 'photo_to_ukiyoe':
+            transformed_tensor = photo_to_ukiyoe(img_tensor)
+        else:
+            transformed_tensor = ukiyoe_to_photo(img_tensor)
     
-    # NumPy配列をPIL Imageに変換
-    transformed_img = Image.fromarray(transformed_img_array.astype('uint8'))
+    transformed_array = postprocess_image(transformed_tensor)
+    transformed_img = Image.fromarray(transformed_array.astype('uint8'))
     
-    # 変換された画像をバイトストリームに変換
     img_byte_arr = io.BytesIO()
     transformed_img.save(img_byte_arr, format='PNG')
     img_byte_arr.seek(0)
